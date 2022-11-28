ea34a0f7d673f5a320909aaccad697f1d1497f05
pankazakos <pankazakos01@gmail.com>
2022-11-28 22:21:51 +0200

    initial code

---
+++ examples/hp_main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "bf.h"
+#include "hp_file.h"
+
+#define RECORDS_NUM 1000 // you can change it if you want
+#define GLOBAL_DEPT 2 // you can change it if you want
+#define FILE_NAME "data.db"
+
+#define CALL_OR_DIE(call)     \
+  {                           \
+    BF_ErrorCode code = call; \
+    if (code != BF_OK) {      \
+      BF_PrintError(code);    \
+      exit(code);             \
+    }                         \
+  }
+
+int main() {
+  BF_Init(LRU);
+
+  HP_CreateFile("data.db",ID);
+  HP_info* info = HP_OpenFile("data.db");
+
+  Record record;
+  srand(12569874);
+  int r;
+  printf("Insert Entries\n");
+  for (int id = 0; id < RECORDS_NUM; ++id) {
+    record = randomRecord();
+    //printRecord(record);
+    HP_InsertEntry(*info, record);
+  }
+
+  printf("RUN PrintAllEntries\n");
+  int id = rand() % RECORDS_NUM;
+  HP_GetAllEntries(* info, &id);
+
+  HP_CloseFile(* info);
+  BF_Close();
+}
---
+++ src/hp_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "bf.h"
+#include "hp_file.h"
+#define MAX_OPEN_FILES 20
+
+#define CALL_BF(call)       \
+{                           \
+  BF_ErrorCode code = call; \
+  if (code != BF_OK) {         \
+    BF_PrintError(code);    \
+    return HP_ERROR;        \
+  }                         \
+}
+
+int HP_CreateFile(char *fileName, Record_Attribute attr){
+    return -1;
+}
+
+HP_info* HP_OpenFile(char *fileName){
+    return malloc(sizeof(HP_info));
+
+}
+
+
+int HP_CloseFile( HP_info header_info ){
+    return -1;
+}
+
+int HP_InsertEntry(HP_info header_info, Record record){
+        return -1;
+}
+
+int HP_GetAllEntries(HP_info header_info, void *value ){
+    return -1;
+}
+
